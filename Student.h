19a57305dad89fd9454bcd44b0974e0f0cbecd91
sanchezjrarturo21 <46791369+sanchezjrarturo21@users.noreply.github.com>
2019-03-26 20:47:24 -0700

    made a class and renamed files to Student

---
+++ Student.h
@@ -0,0 +1,42 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+Class Student{
+  private:
+    string name;
+    int id;
+    double average;
+    char grade;
+    int rank;
+  public:
+    Student();
+    void setName(string name);
+    void setID(int id);
+    void setAverage(int average);
+    void setGrade(char grade);
+    void setRank(int rank);
+    string getName() const;
+    int getID() const;
+    double getAverage() const;
+    char getGrade() const;
+    int getRank() const;
+    void print() const;
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
