2abad3d8d871e1ce07dab902fab479682f73f825
sanchezjrarturo21 <46791369+sanchezjrarturo21@users.noreply.github.com>
2019-03-26 20:53:06 -0700

    yeet

--- Student.cpp
+++ Student.cpp
@@ -1,2 +1,60 @@
 #include <iostream>
 using namespace std;
+
+Student::Student(){
+
+
+}
+
+void Student::setName(string name){
+
+
+}
+
+void Student::setID(int id){
+
+
+}
+
+void Student::setAverage(int average){
+
+
+}
+
+void Student::setGrade(char grade){
+
+
+}
+
+void Student::setRank(int rank){
+
+
+}
+
+string Student::getName() const{
+
+
+}
+
+int Student::getID() const{
+
+
+}
+double Student::getAverage() const{
+
+
+}
+char Student::getGrade() const{
+
+
+}
+
+int Student::getRank() const{
+
+
+}
+
+void Student::print() const{
+
+
+}
